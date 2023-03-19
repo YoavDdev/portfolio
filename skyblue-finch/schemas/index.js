a35f7b517954d1c653983a6b0416dcc7576081da
Yoav drai <yoavDdev@gmail.com>
2023-03-19 11:12:43 +0200

    no DB

--- skyblue-finch/schemas/index.js
+++
@@ -1,7 +0,0 @@
-import pageInfo from './pageInfo'
-import skill from './skill'
-import expirience from './experience'
-import social from './social'
-import project from './project'
-
-export const schemaTypes = [pageInfo, skill, expirience, social, project]
